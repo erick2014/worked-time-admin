9a9ddf1f48732aa097073d86ab2cd28a7ffa5f71
erick <erickarmandogarcia@hotmail.com>
2017-05-26 17:40:15 -0500

    Add fetch.js helper library to fetch data from remote server

---
+++ src_users/api/fetch.js
@@ -0,0 +1,35 @@
+import fetch from 'isomorphic-fetch'
+const host = 'timesheet-staging-aurity.herokuapp.com'
+
+function buildHeaders(optionalHeaders = {}) {
+  return new Headers({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    ...optionalHeaders
+  })
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+export const apiURL = `https://${host}/api`
+
+export async function httpGet(url) {
+  const response = await fetch(url, {method:"GET", headers: buildHeaders() })
+
+  return await checkStatus(response).json()
+}
+
+export async function httPUT(url, data) {
+  const body = JSON.stringify(data)
+  const response = await fetch(url, {method: 'PUT',headers: buildHeaders(),body: body })
+
+  return await checkStatus(response).json()
+}
